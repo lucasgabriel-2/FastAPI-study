883d3e0756744c7cf8f571f92769d30f81639af7
Lucas Gabriel <lucasgabriel_df@hotmail.com>
2024-12-20 14:02:39 -0300

    Iniciando o projeto - ajustes

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -7,6 +7,8 @@ from fast_zero.app import app
 
 def test_read_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)
+
     response = client.get('/')
+
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
