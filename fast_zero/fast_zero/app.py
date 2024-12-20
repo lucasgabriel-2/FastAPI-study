4e78a199c0e2aafdaf72136845550ca173bcb521
Lucas Gabriel <lucasgabriel_df@hotmail.com>
2024-12-20 17:02:22 -0300

    Aula 02 - exercicio

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -8,3 +9,16 @@ app = FastAPI()
 @app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/HTMLRequest', response_class=HTMLResponse)
+def return_an_html_with_hello_world():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -2,8 +2,6 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from fast_zero.schemas import Message
-
 from fast_zero.app import app
 
 
@@ -14,3 +12,12 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_should_return_an_html_with_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/HTMLRequest')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
